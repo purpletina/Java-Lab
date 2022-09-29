9e46386c24eebf43ba6eba9f006990bc13523c94
purpletina <113041666+purpletina@users.noreply.github.com>
2022-09-30 00:53:03 +0800

    Create Class02.java

---
+++ Class02.java
@@ -0,0 +1,36 @@
+/**
+ * 
+ */
+package hw;
+class Cbbb
+{
+	double x;
+	double y;
+}
+/**
+ * @author CJCU-User
+ *
+ */
+public class hw1 {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Cbbb obj1 = new Cbbb();
+		Cbbb obj2 = new Cbbb();
+		Cbbb avg = new Cbbb();
+		obj1.x = 5.2;
+		obj1.y = 3.9;
+		obj2.x = 6.5;
+		obj2.y = 4.6;
+		avg.x = (obj1.x + obj2.x)/2;
+		avg.y = (obj1.y + obj2.y)/2;
+		
+		System.out.println("obj1.x = " + obj1.x +", obj1.y = " + obj1.y);
+		System.out.println("obj2.x = " + obj2.x + ", obj2.y = " + obj2.y);
+		System.out.println("avg.x = " + avg.x + ", avg.y = " + avg.y);
+	}
+
+}
