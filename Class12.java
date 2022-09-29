1530b357225a2c81dd119a0c98955d235934d5c5
purpletina <113041666+purpletina@users.noreply.github.com>
2022-09-30 00:58:20 +0800

    Create Class12.java

---
+++ Class12.java
@@ -0,0 +1,49 @@
+/**
+ * 
+ */
+package hw1;
+
+class CWin {
+	int width;
+	int height;
+	String name;
+
+	void setWindows(int w, int h) {
+		width = w;
+		height = h;
+	}
+
+	void setWindows(int w, int h, String s) {
+		width = w;
+		height = h;
+		name = s;
+	}
+
+	void setW(int w) {
+		width = w;
+	}
+
+	void setH(int h) {
+		height = h;
+	}
+
+	void setName(String s) {
+		name = s;
+	}
+
+	public void show() {
+		System.out.println("Name=" + name);
+		System.out.println("W=" + width + ", H=" + height);
+	}
+}
+	public class hw7 {
+
+		public static void main(String[] args) {
+			// TODO Auto-generated method stub
+			CWin cw=new CWin();
+	         cw.setName("My Windows");
+	         cw.setW(5);
+	         cw.setH(3);
+	         cw.show();
+		}			
+}
